1b6f3d5f3bce0efb1970498002cf6e373b65cd93
yeooonee <skylarlee1003@gmail.com>
2026-09-21 17:29:26 +0900

    05 solve

--- challenges/05_null_return_deref/bug.c
+++ challenges/05_null_return_deref/bug.c
@@ -64,7 +64,12 @@ static void expand(const Config *c, const char *tmpl, char *out, size_t outcap) 
             memcpy(key, p + 2, kl);
             key[kl] = '\0';
 
-            const char *v = cfg_get(c, key);      
+            const char *v = cfg_get(c, key);   
+
+            if (v == NULL){
+                v = "";
+            }
+
             size_t vl = strlen(v);                 
             if (o + vl < outcap) { memcpy(out + o, v, vl); o += vl; }
             p = end + 1;
